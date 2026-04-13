5704b76dd4b8929d506d55e5dde80dd686dcdb07
tumobilamigo-beep <tumobilamigo@gmail.com>
2026-04-13 08:01:38 -0500

    api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-// js/api.js
+// El Motor de Datos: Unico que habla con Supabase. Centraliza las llaves y la conexión
 const SUPABASE_URL = "https://fsdqvdxvhqauyqczgsfb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZHF2ZHh2aHFhdXlxY3pnc2ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NjU0OTIsImV4cCI6MjA5MDU0MTQ5Mn0.AA7CgSig_lgXSwVeZex-C1AExRKgN1sMuIBnzH9pfqM";
 
